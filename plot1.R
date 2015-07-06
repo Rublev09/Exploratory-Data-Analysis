9d07550c2ec752a91af57c65dee1b23369e33ab3
Aki Paliotta <Rublev09@users.noreply.github.com>
2015-07-06 19:31:09 +0200

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
 
-# The dataset has 2,075,259 rows and 9 columns. A rough estimate of the memory requirements is 2M*9*8bytes = 144MB.
+# The dataset has 2,075,259 rows and 9 columns. A rough estimate of the memory requirements is 2M*9*8bytes = 144MB
 
+# Load file
 data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.string="?")
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 
@@ -8,7 +9,7 @@ data$Date <- as.Date(data$Date, format="%d/%m/%Y")
 newdata <- subset(data, Date=="2007-02-01" | Date=="2007-02-02") 
 rm(data)
 
-# Convert Date and Time Vb using strptime() and as.Date() functions
+# Convert Date and Time Vb with strptime() and as.Date()
 datetime <- paste(as.Date(newdata$Date), newdata$Time)
 newdata$Datetime <- strptime(datetime, format="%Y-%m-%d %H:%M:%S", tz="")
 
