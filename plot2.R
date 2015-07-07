e233052ad7c964e4c944e818ea1b4a492154ceb8
Aki Paliotta <rublev09@gmail.com>
2015-07-07 13:36:34 +0200

    add png file

---
+++ plot2.R
@@ -0,0 +1,22 @@
+#EDA 
+# Dataset has 2,075,259 rows and 9 columns. A rough estimate of the memory requirements is 2M*9*8bytes=144MB
+
+# Load file
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.string="?")
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+
+# Get only Vb: 1st or 2nd February, 2007
+newdata <- subset(data, Date=="2007-02-01" | Date=="2007-02-02") 
+rm(data)
+
+# Convert Date and Time Vb with strptime() and as.Date() 
+datetime <- paste(as.Date(newdata$Date), newdata$Time)
+newdata$Datetime <- strptime(datetime, format="%Y-%m-%d %H:%M:%S", tz="")
+
+# Line chart
+plot(newdata$Datetime, newdata$Global_active_power, 
+     type="l", ylab="Global Active Power (kilowatts)", xlab="")                                                                                   
+
+# Save file .png 480x480
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
